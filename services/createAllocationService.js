a31663ba1d60ca14ee6fc040fcc244bcd62b635c
Joshua Ramirez <joshua.ramirez@me.com>
2019-04-26 13:29:28 +0200

    Upgraded allocation service to support a saga scenario w/ transactions.

--- services/createAllocationService.js
+++ services/createAllocationService.js
@@ -1,18 +1,52 @@
 import {allocationProjectionStore} from "../projections/allocationProjectionStore";
+import {newTransactionCreatedPublisher} from "../publishers/newTransactionCreatedPublisher";
+import {newAllocationSubmittedPublisher} from "../publishers/newAllocationSubmittedPublisher";
+import {newTransactionSubmittedPublisher} from "../publishers/newTransactionSubmittedPublisher";
+import {sagaStore} from "../data/sagaStore";
 
 const factory = () => {
 
   const process = (parameters) => {
 
-    const createAllocationProjection = () => {
+    const sagaName = newAllocationSubmittedPublisher.eventName;
+
+    const createAllocationProjection = (saga) => {
+      const originalEvent = saga.sagaData.originalEvent;
       const newAllocation = allocationProjectionStore.project.contract();
-      newAllocation.amount = parameters.amount;
-      newAllocation.ledgerId = parameters.ledgerId;
+      newAllocation.amount = originalEvent.amount;
+      newAllocation.ledgerId = originalEvent.ledgerId;
+      newAllocation.transactionId = saga.sagaData.transactionId;
       allocationProjectionStore.project(newAllocation);
     };
 
-    createAllocationProjection();
+    const submitNewTransaction = (saga) => {
+      const eventData = newTransactionSubmittedPublisher.publish.contract();
+      eventData.amount = parameters.amount;
+      eventData.destination = parameters.destination;
+      eventData.ledgerId = parameters.ledgerId;
+      eventData.source = "Allocation";
+      eventData.type = "Allocation";
+      if (saga) eventData.sagaId = saga.sagaId;
+      newTransactionSubmittedPublisher.publish(eventData);
+    };
 
+    //New Allocation Submitted
+    if(parameters.eventName === newAllocationSubmittedPublisher.eventName){
+      const sagaData = {
+        originalEvent: parameters
+      };
+      const saga = sagaStore.saveSaga(sagaName, sagaData);
+      submitNewTransaction(saga);
+    }
+
+    //New Transaction Created
+    if(parameters.eventName === newTransactionCreatedPublisher.eventName){
+      if (!parameters.sagaId) return;
+      const sagaId = parameters.sagaId; //TODO: Standardize ID Names
+      const saga = sagaStore.getSaga(sagaName, sagaId);
+      saga.sagaData.transactionId = parameters.transaction.id; //TODO: Standardize ID Names
+      createAllocationProjection(saga);
+    }
 
   };
 
