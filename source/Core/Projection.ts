714544dcfcca73f88dea5d5d93f062284edcb544
Joshua Ramirez <joshua.ramirez@me.com>
2019-04-29 22:21:39 +0200

    Generating slightly unique Id values for testing.

--- source/Core/Projection.ts
+++ source/Core/Projection.ts
@@ -4,6 +4,7 @@ export abstract class Projection implements IProjection {
   public Id: any;
   public ProjectionName: string;
   protected constructor(projectionName: string) {
+    this.Id = Math.random().toString(36).substr(2, 9);
     this.ProjectionName = projectionName;
   }
 
