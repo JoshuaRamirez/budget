cea7cfe4d48f67499c0449c6ab9c5285f12234e2
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-01 15:36:20 +0200

    Adding TestEventStore

---
+++ test/unit/Core/TestEventStore.ts
@@ -0,0 +1,47 @@
+import { assert } from "chai";
+import "mocha";
+import { EventStore } from "../../../source/Core/EventStore";
+import { AccountRequestedEvent } from "../../../source/Events/AccountRequestedEvent";
+import { AllocationRequestedEvent } from "../../../source/Events/AllocationRequestedEvent";
+import { ExpenseCreatedEvent } from "../../../source/Events/ExpenseCreatedEvent";
+import { LedgerRequestedEvent } from "../../../source/Events/LedgerRequestedEvent";
+import { PayeeCreatedEvent } from "../../../source/Events/PayeeCreatedEvent";
+import { PlannedExpenseRequestedEvent } from "../../../source/Events/PlannedExpenseRequestedEvent";
+import { TransactionCreatedEvent } from "../../../source/Events/TransactionCreatedEvent";
+import { TransactionSubmittedEvent } from "../../../source/Events/TransactionSubmittedEvent";
+
+describe("EventStore", () => {
+  let eventStore: EventStore;
+  beforeEach(() => {
+    eventStore = new EventStore();
+  });
+  it("should instantiate.", () => {
+    assert.exists(eventStore);
+  });
+  describe("should without any exceptions", () => {
+    it("record AccountRequestedEvent", () => {
+      eventStore.Record<AccountRequestedEvent>(new AccountRequestedEvent());
+    });
+    it("should record AllocationRequestedEvent", () => {
+      eventStore.Record<AllocationRequestedEvent>(new AllocationRequestedEvent());
+    });
+    it("should record ExpenseCreatedEvent", () => {
+      eventStore.Record<ExpenseCreatedEvent>(new ExpenseCreatedEvent());
+    });
+    it("should record LedgerRequestedEvent", () => {
+      eventStore.Record<LedgerRequestedEvent>(new LedgerRequestedEvent());
+    });
+    it("should record PayeeCreatedEvent", () => {
+      eventStore.Record<PayeeCreatedEvent>(new PayeeCreatedEvent());
+    });
+    it("should record PlannedExpenseRequestedEvent", () => {
+      eventStore.Record<PlannedExpenseRequestedEvent>(new PlannedExpenseRequestedEvent());
+    });
+    it("should record TransactionCreatedEvent", () => {
+      eventStore.Record<TransactionCreatedEvent>(new TransactionCreatedEvent());
+    });
+    it("should record TransactionSubmittedEvent", () => {
+      eventStore.Record<TransactionSubmittedEvent>(new TransactionSubmittedEvent());
+    });
+  });
+});
