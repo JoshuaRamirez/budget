1efbd693319c8035170e7d526d3a2027cdbefcd8
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-01 14:35:28 +0200

    Adding TestCreateAllocationService.

---
+++ test/Services/TestCreateAllocationService.ts
@@ -0,0 +1,15 @@
+import { assert } from "chai";
+import "mocha";
+import {CreateAllocationService} from "../../source/Services/CreateAllocationService";
+
+describe("CreateAllocationService", () => {
+
+  it("should import", () => {
+    assert.exists(CreateAllocationService);
+  });
+  it("should instantiate", () => {
+    const createAllocationService = new CreateAllocationService();
+    assert.exists(createAllocationService);
+  });
+
+});
