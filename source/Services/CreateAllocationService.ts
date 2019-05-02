8975772aad3ace431f5f28650d88500d65c7f58d
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-02 14:27:04 +0200

    Switching up on the style, perhaps this is easier to read.

--- source/Services/CreateAllocationService.ts
+++ source/Services/CreateAllocationService.ts
@@ -10,18 +10,15 @@ export class CreateAllocationService implements ISubscriber<TransactionCreatedEv
   public static Instance = new CreateAllocationService();
   public Process(event: TransactionCreatedEvent) {
     if (!event.SagaId) { return; }
-    const createAllocationProjection = () => {
-      const originalEvent: TransactionSubmittedEvent = saga.sagaData.originalEvent;
-      const allocationProjection: AllocationProjection = new AllocationProjection();
-      allocationProjection.Amount = originalEvent.Amount;
-      allocationProjection.LedgerId = originalEvent.LedgerId;
-      allocationProjection.TransactionId = saga.sagaData.transactionId;
-      ProjectionStore.Instance.Project(allocationProjection);
-    };
     const sagaName = AllocationRequestedEvent.name;
     const sagaId = event.SagaId; // TODO: Standardize ID Names
     const saga = SagaStore.Instance.GetSaga(sagaName, sagaId);
     saga.sagaData.transactionId = event.Transaction.Id; // TODO: Standardize ID Names
-    createAllocationProjection();
+    const originalEvent: TransactionSubmittedEvent = saga.sagaData.originalEvent;
+    const allocationProjection: AllocationProjection = new AllocationProjection();
+    allocationProjection.Amount = originalEvent.Amount;
+    allocationProjection.LedgerId = originalEvent.LedgerId;
+    allocationProjection.TransactionId = saga.sagaData.transactionId;
+    ProjectionStore.Instance.Project(allocationProjection);
   }
 }
