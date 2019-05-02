1c4d15a9c5f063d620b3e49e2f1bc07cc28d9f2f
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-02 13:07:45 +0200

    Converted TestForSmoke to use BDD with Gherkin Syntax

--- test/scenarios/TestForSmoke.ts
+++ test/scenarios/TestForSmoke.ts
@@ -14,33 +14,83 @@ import {
 
 const projectionStore = ProjectionStore.Instance;
 
-describe("CreateAllocationService", () => {
+describe("Scenarios", () => {
 
-  it("smoke test passes", () => {
-    const subscriptions = new Subscriptions();
+  describe("Features", () => {
 
-    subscriptions.Create();
-    PublishNewAccountSubmitted();
-    const ledger = projectionStore.GetProjections(LedgerProjection)[0];
-    PublishNewTransaction(1);
-    assert.equal(ledger.Balance, -1);
-    PublishNewTransaction(1);
-    assert.equal(ledger.Balance, -2);
-    PublishNewTransaction(1);
-    assert.equal(ledger.Balance, -3);
-    PublishNewTransaction(1);
-    assert.equal(ledger.Balance, -4);
-    PublishNewTransaction(1);
-    assert.equal(ledger.Balance, -5);
-    PublishNewAllocation(ledger.Id, -10);
-    const allocationProjections = projectionStore.GetProjections(AllocationProjection);
-    assert.equal(ledger.Balance, 5);
-    assert.equal(allocationProjections.length, 1);
-    assert.equal(allocationProjections[0].LedgerId, ledger.Id);
-    RequestPlannedExpense();
-    const plannedExpense = projectionStore.GetProjections(PlannedExpenseProjection)[0];
-    assert.exists(plannedExpense);
-    assert.equal(ledger.TransactionIds.length, 6);
+    describe("Basic Usage", () => {
+      let allocationProjections;
+      let subscriptions;
+      let ledger;
+      it("Given all subscriptions are setup", () => {
+        subscriptions = new Subscriptions();
+        subscriptions.Create();
+      });
+      it("When a NewAccountSubmitted event is published", () => {
+        PublishNewAccountSubmitted();
+      });
+      it("Then a Ledger should be created", () => {
+        ledger = projectionStore.GetProjections(LedgerProjection)[0];
+        assert.exists(ledger);
+      });
+      it("And the ledger should have a zero balance", () => {
+        assert.isTrue(ledger.Balance === 0);
+      });
+      it("When a $1 withdrawal transaction is created;", () => {
+        PublishNewTransaction(1);
+      });
+      it("Then the Ledger balance should be -1", () => {
+        assert.equal(ledger.Balance, -1);
+      });
+      it("When another $1 withdrawal transaction is created;", () => {
+        PublishNewTransaction(1);
+      });
+      it("Then the Ledger balance should be -2", () => {
+        assert.equal(ledger.Balance, -2);
+      });
+      it("When another $1 withdrawal transaction is created;", () => {
+        PublishNewTransaction(1);
+      });
+      it("Then the Ledger balance should be -3", () => {
+        assert.equal(ledger.Balance, -3);
+      });
+      it("When another $1 withdrawal transaction is created;", () => {
+        PublishNewTransaction(1);
+      });
+      it("Then the Ledger balance should be -4", () => {
+        assert.equal(ledger.Balance, -4);
+      });
+      it("When another $1 withdrawal transaction is created;", () => {
+        PublishNewTransaction(1);
+      });
+      it("Then the Ledger balance should be -5", () => {
+        assert.equal(ledger.Balance, -5);
+      });
+      it("When a new $10 Allocation is made to the Ledger", () => {
+        PublishNewAllocation(ledger.Id, -10);
+      });
+      it("Then the Ledger balance should be 5", () => {
+        assert.equal(ledger.Balance, 5);
+      });
+      it("And the Ledger should be linked to 6 Transactions", () => {
+        assert.equal(ledger.TransactionIds.length, 6);
+      });
+      it("And the Allocation should have been created", () => {
+        allocationProjections = projectionStore.GetProjections(AllocationProjection);
+        assert.equal(allocationProjections.length, 1);
+      });
+      it("And the Allocation should be linked to the Ledger", () => {
+        assert.equal(allocationProjections[0].LedgerId, ledger.Id);
+      });
+      it("When a new Planned Expense is requested", () => {
+        RequestPlannedExpense();
+      });
+      it("Then a new PlannedExpenseProjection should exist", () => {
+        const plannedExpense = projectionStore.GetProjections(PlannedExpenseProjection)[0];
+        assert.exists(plannedExpense);
+      });
+    });
+
   });
 
 });
