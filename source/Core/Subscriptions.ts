2cec72f8741a8933e48ad97087c7da07e4d1374e
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-04 23:01:46 +0200

    Added subscriptions for creating expenses.

--- source/Core/Subscriptions.ts
+++ source/Core/Subscriptions.ts
@@ -1,6 +1,8 @@
 import { CreateAccountService } from "../Services/CreateAccountService";
 import { CreateAllocationService } from "../Services/CreateAllocationService";
 import { CreateAllocationTransactionService } from "../Services/CreateAllocationTransactionService";
+import { CreateExpenseService } from "../Services/CreateExpenseService";
+import { CreateExpenseTransactionService } from "../Services/CreateExpenseTransactionService";
 import { CreateLedgerService } from "../Services/CreateLedgerService";
 import { CreatePlannedExpenseService } from "../Services/CreatePlannedExpenseService";
 import { CreateTransactionService } from "../Services/CreateTransactionService";
@@ -13,5 +15,7 @@ export class Subscriptions {
     CreateAllocationService.Instance.Subscribe();
     CreateTransactionService.Instance.Subscribe();
     CreatePlannedExpenseService.Instance.Subscribe();
+    CreateExpenseService.Instance.Subscribe();
+    CreateExpenseTransactionService.Instance.Subscribe();
   }
 }
