2342c24048c08f1ae8485961c4d577040a2c998b
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-04 23:45:01 +0200

    Adding PublishPayeeRequested function.

--- test/scenarios/Helpers.ts
+++ test/scenarios/Helpers.ts
@@ -2,6 +2,7 @@ import { ProjectionStore } from "../../source/Core/ProjectionStore";
 import { AccountRequestedEvent } from "../../source/Events/AccountRequestedEvent";
 import { AllocationRequestedEvent } from "../../source/Events/AllocationRequestedEvent";
 import { ExpenseRequestedEvent } from "../../source/Events/ExpenseRequestedEvent";
+import { PayeeRequestedEvent } from "../../source/Events/PayeeRequestedEvent";
 import { PlannedExpenseRequestedEvent } from "../../source/Events/PlannedExpenseRequestedEvent";
 import { TransactionSubmittedEvent } from "../../source/Events/TransactionSubmittedEvent";
 import { LedgerProjection } from "../../source/Projections/LedgerProjection";
@@ -44,6 +45,13 @@ export const PublishExpenseRequested = (amount, ledgerId, plannedExpenseId = nul
   expenseRequestedEvent.PayeeId = payeeId;
   expenseRequestedEvent.PlannedExpenseId = plannedExpenseId;
   expenseRequestedEvent.Publish();
+};
+
+export const PublishPayeeRequested = () => {
+  const payeeRequestedEvent = new PayeeRequestedEvent();
+  payeeRequestedEvent.Description = "Test";
+  payeeRequestedEvent.Name = Date.now().toString();
+  payeeRequestedEvent.Type = "Test";
 };
 
 export const RequestPlannedExpense = () => {
