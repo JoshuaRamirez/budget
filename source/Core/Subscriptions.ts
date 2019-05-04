be9c3948c083d235238b0d594900feb4fadb6fb0
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-04 19:21:14 +0200

    Using updated Subscription idiom.

--- source/Core/Subscriptions.ts
+++ source/Core/Subscriptions.ts
@@ -1,25 +1,17 @@
-import { AccountRequestedEvent } from "../Events/AccountRequestedEvent";
-import { AllocationRequestedEvent } from "../Events/AllocationRequestedEvent";
-import { LedgerRequestedEvent } from "../Events/LedgerRequestedEvent";
-import { PlannedExpenseRequestedEvent } from "../Events/PlannedExpenseRequestedEvent";
-import { TransactionCreatedEvent } from "../Events/TransactionCreatedEvent";
-import { TransactionSubmittedEvent } from "../Events/TransactionSubmittedEvent";
 import { CreateAccountService } from "../Services/CreateAccountService";
 import { CreateAllocationService } from "../Services/CreateAllocationService";
 import { CreateAllocationTransactionService } from "../Services/CreateAllocationTransactionService";
 import { CreateLedgerService } from "../Services/CreateLedgerService";
 import { CreatePlannedExpenseService } from "../Services/CreatePlannedExpenseService";
 import { CreateTransactionService } from "../Services/CreateTransactionService";
-import { Publisher } from "./Publisher";
 
 export class Subscriptions {
   public Create() {
-    const publisher = Publisher.Instance;
-    publisher.Subscribe(AccountRequestedEvent, CreateAccountService.Instance);
-    publisher.Subscribe(AllocationRequestedEvent, CreateAllocationTransactionService.Instance);
-    publisher.Subscribe(LedgerRequestedEvent, CreateLedgerService.Instance);
-    publisher.Subscribe(TransactionCreatedEvent, CreateAllocationService.Instance);
-    publisher.Subscribe(TransactionSubmittedEvent, CreateTransactionService.Instance);
-    publisher.Subscribe(PlannedExpenseRequestedEvent, CreatePlannedExpenseService.Instance);
+    CreateAccountService.Instance.Subscribe();
+    CreateAllocationTransactionService.Instance.Subscribe();
+    CreateLedgerService.Instance.Subscribe();
+    CreateAllocationService.Instance.Subscribe();
+    CreateTransactionService.Instance.Subscribe();
+    CreatePlannedExpenseService.Instance.Subscribe();
   }
 }
