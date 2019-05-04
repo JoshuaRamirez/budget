d7fb81ec7776208779ded6fb4c67ba8b06e31c58
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-04 20:08:33 +0200

    Making constructor protected

--- source/Core/Saga.ts
+++ source/Core/Saga.ts
@@ -3,7 +3,7 @@ import { Id } from "./Id";
 export abstract class Saga {
   public Id: any;
   public Name: string;
-  public constructor(sagaName: string) {
+  protected constructor(sagaName: string) {
     this.Name = sagaName;
     this.Id = Id.Generate();
   }
