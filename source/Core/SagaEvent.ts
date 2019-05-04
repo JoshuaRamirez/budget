3801624785169e591d70bbb31c90f0803cab4139
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-04 22:19:35 +0200

    Adding TODO

--- source/Core/SagaEvent.ts
+++ source/Core/SagaEvent.ts
@@ -1,6 +1,7 @@
 import { MainEvent } from "./MainEvent";
 
 export abstract class SagaEvent<TEvent extends SagaEvent<TEvent>> extends MainEvent<TEvent> {
+  // TODO: Create a constructor that takes in the SagaId and SagaName
   public SagaId: any;
   public SagaName: string;
   public abstract Publish();
