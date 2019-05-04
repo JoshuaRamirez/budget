158208a42df06ecd5553a28311a18463bdcb6413
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-04 22:16:33 +0200

    Adding SagaName to SagaEvent

--- source/Core/SagaEvent.ts
+++ source/Core/SagaEvent.ts
@@ -2,5 +2,6 @@ import { MainEvent } from "./MainEvent";
 
 export abstract class SagaEvent<TEvent extends SagaEvent<TEvent>> extends MainEvent<TEvent> {
   public SagaId: any;
+  public SagaName: string;
   public abstract Publish();
 }
