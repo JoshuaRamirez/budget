5df895a2361fb9fbfe08f35513d34d535457f70b
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-04 22:32:16 +0200

    Making constructor protected

--- source/Core/MainEvent.ts
+++ source/Core/MainEvent.ts
@@ -5,7 +5,7 @@ import { IEvent } from "./IEvent";
 export abstract class MainEvent<TEvent extends MainEvent<TEvent>> implements IEvent {
   public EventId: any = Id.Generate();
   public EventName: string;
-  constructor(eventName: string) {
+  protected constructor(eventName: string) {
     this.EventName = eventName;
   }
   public abstract Publish();
