112545922650b07835c7b6dcb1acfb5e7de53c71
Joshua Ramirez <joshua.ramirez@me.com>
2019-05-05 10:58:44 +0200

    Added LinkPayeeToExpenseService

---
+++ source/Services/LinkPayeeToExpenseService.ts
@@ -0,0 +1,20 @@
+import { ISubscriber } from "../Core/ISubscriber";
+import { Publisher } from "../Core/Publisher";
+import { ExpenseCreatedEvent } from "../Events/ExpenseCreatedEvent";
+import { PayeeProjection } from "../Projections/PayeeProjection";
+
+export class LinkPayeeToExpenseService implements ISubscriber<ExpenseCreatedEvent> {
+  public Process(event: ExpenseCreatedEvent): void {
+    if (!event.ExpenseProjection.PayeeId) {
+      return;
+    }
+    const payeeProjection = PayeeProjection.Get(event.ExpenseProjection.PayeeId);
+    if (!payeeProjection) {
+      throw new Error("The ProjectionStore returns no valid PayeeProjection associated with the Saga's PayeeId.");
+    }
+    payeeProjection.ExpenseIds.push(payeeProjection.Id);
+  }
+  public Subscribe() {
+    Publisher.Instance.Subscribe(ExpenseCreatedEvent, this);
+  }
+}
