d0834cabd53ab583894cfbf6353a3acbc7bbb8cc
Joshua Ramirez <joshua.ramirez@me.com>
2019-06-18 19:38:22 +0900

    Created CreateCategoryService.

---
+++ test/unit/Services/CreateCategoryService.ts
@@ -0,0 +1,23 @@
+import { assert } from "chai";
+import "mocha";
+import { CategoryRequestedEvent } from "../../../source/Events/CategoryRequestedEvent";
+import {CreateCategoryService} from "../../../source/Services/CreateCategoryService";
+
+describe("CreateCategoryService", () => {
+  it("should import", () => {
+    assert.exists(CreateCategoryService);
+  });
+  it("should instantiate", () => {
+    const service = new CreateCategoryService();
+    assert.exists(service);
+  });
+  it("should instantiate with singleton", () => {
+    const service = CreateCategoryService.Instance;
+    assert.exists(service);
+  });
+  it("should process", () => {
+    const service = CreateCategoryService.Instance;
+    const event = new CategoryRequestedEvent();
+    service.Process(event);
+  });
+});
