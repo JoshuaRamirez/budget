da4542b7bc90230db97a6bee544a46d86e031496
Joshua Ramirez <joshua.ramirez@me.com>
2019-06-23 18:11:54 +0900

    Renamed file and added singleton/process tests.

---
+++ test/unit/Services/CreateAllocationService.ts
@@ -0,0 +1,23 @@
+import { assert } from "chai";
+import "mocha";
+import { TransactionCreatedEvent } from "../../../source/Events/TransactionCreatedEvent";
+import {CreateAllocationService} from "../../../source/Services/CreateAllocationService";
+
+describe("CreateAllocationService", () => {
+  it("should import", () => {
+    assert.exists(CreateAllocationService);
+  });
+  it("should instantiate", () => {
+    const service = new CreateAllocationService();
+    assert.exists(service);
+  });
+  it("should instantiate with singleton", () => {
+    const service = CreateAllocationService.Instance;
+    assert.exists(service);
+  });
+  it("should process", () => {
+    const service = CreateAllocationService.Instance;
+    const event = new TransactionCreatedEvent("a", "1");
+    service.Process(event);
+  });
+});
