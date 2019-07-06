667d4926670fa60b63a34650c869295ee42100fd
Joshua Ramirez <joshua.ramirez@me.com>
2019-07-06 18:36:23 +0900

    Created AllocationRequestedEvent unit test file.

---
+++ test/unit/Events/AllocationRequestedEvent.ts
@@ -0,0 +1,17 @@
+import { assert } from "chai";
+import "mocha";
+import {AllocationRequestedEvent} from "../../../source/Events/AllocationRequestedEvent";
+
+describe("AllocationRequestedEvent", () => {
+  it("should import", () => {
+    assert.exists(AllocationRequestedEvent);
+  });
+  it("should instantiate", () => {
+    const event = new AllocationRequestedEvent();
+    assert.exists(event);
+  });
+  it("should publish", () => {
+    const event = new AllocationRequestedEvent();
+    event.Publish();
+  });
+});
