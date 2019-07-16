98a64f1c11efae0fe172aa47563261e81badf1da
Joshua Ramirez <joshua.ramirez@me.com>
2019-07-16 18:08:35 -0500

    Added CreateExpenseService collaboration test.

---
+++ test/collaboration/Services/CreateExpenseService.ts
@@ -0,0 +1,27 @@
+import { assert } from "chai";
+import "mocha";
+import { ProjectionStore } from "../../../source/Core/ProjectionStore";
+import { ExpenseRequestedEvent } from "../../../source/Events/ExpenseRequestedEvent";
+import { TransactionCreatedEvent } from "../../../source/Events/TransactionCreatedEvent";
+import { ExpenseProjection } from "../../../source/Projections/ExpenseProjection";
+import { TransactionProjection } from "../../../source/Projections/TransactionProjection";
+import { CreateExpenseSaga } from "../../../source/Sagas/CreateExpenseSaga";
+import { CreateExpenseService } from "../../../source/Services/CreateExpenseService";
+
+
+describe("CreateExpenseService", () => {
+  it("should create projection", () => {
+    const service = CreateExpenseService.Instance;
+    service.Subscribe();
+    const allocationRequestedEvent = new ExpenseRequestedEvent();
+    const saga = new CreateExpenseSaga(allocationRequestedEvent);
+    saga.Save();
+    const transactionCreatedEvent = new TransactionCreatedEvent(saga.Name, saga.Id);
+    transactionCreatedEvent.Transaction = new TransactionProjection();
+    transactionCreatedEvent.Publish();
+    const projectionStore = ProjectionStore.Instance;
+    const projections = projectionStore.GetProjections(ExpenseProjection);
+    const projection = projections[0];
+    assert.exists(projection);
+  });
+});
