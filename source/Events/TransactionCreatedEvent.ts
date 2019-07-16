117f470ca0cb9802b2296eb05035930cd054778b
Joshua Ramirez <joshua.ramirez@me.com>
2019-07-16 17:56:48 -0500

    Removed unused field.

--- source/Events/TransactionCreatedEvent.ts
+++ source/Events/TransactionCreatedEvent.ts
@@ -3,7 +3,6 @@ import { SagaEvent } from "../Core/SagaEvent";
 import { TransactionProjection } from "../Projections/TransactionProjection";
 
 export class TransactionCreatedEvent extends SagaEvent {
-  public Amount: number;
   public Transaction: TransactionProjection; // TODO: Convert to ID
   constructor(sagaName: string, sagaId: any) { // TODO: Use Saga Class instead.
     super(TransactionCreatedEvent.name, sagaName, sagaId);
