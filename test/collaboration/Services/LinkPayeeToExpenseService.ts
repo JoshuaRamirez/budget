22fa22b57b0cc944778374a71ad192511f37080b
Joshua Ramirez <joshua.ramirez@me.com>
2019-07-16 18:10:18 -0500

    Added LinkPayeeToExpenseService collaboration test.

---
+++ test/collaboration/Services/LinkPayeeToExpenseService.ts
@@ -0,0 +1,27 @@
+import { assert } from "chai";
+import "mocha";
+import { ProjectionStore } from "../../../source/Core/ProjectionStore";
+import { ExpenseCreatedEvent } from "../../../source/Events/ExpenseCreatedEvent";
+import { ExpenseProjection } from "../../../source/Projections/ExpenseProjection";
+import { PayeeProjection } from "../../../source/Projections/PayeeProjection";
+import { LinkPayeeToExpenseService } from "../../../source/Services/LinkPayeeToExpenseService";
+
+
+describe("LinkPayeeToExpenseService", () => {
+  it("should create projection", () => {
+    const service = LinkPayeeToExpenseService.Instance;
+    service.Subscribe();
+    const payeeProjection = new PayeeProjection();
+    payeeProjection.Project();
+    const expenseProjection = new ExpenseProjection();
+    expenseProjection.PayeeId = payeeProjection.Id;
+    expenseProjection.Project();
+    const event = new ExpenseCreatedEvent();
+    event.ExpenseProjection = expenseProjection;
+    event.Publish();
+    const projectionStore = ProjectionStore.Instance;
+    const projections = projectionStore.GetProjections(PayeeProjection);
+    const projection = projections[0];
+    assert.equal(projection.ExpenseIds[0], expenseProjection.Id);
+  });
+});
