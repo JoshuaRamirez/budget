78824539de67a354b284d1da52d116449ce0cccf
Joshua Ramirez <joshua.ramirez@me.com>
2019-07-16 17:56:35 -0500

    Added TODO

--- source/Events/TransactionCreatedEvent.ts
+++ source/Events/TransactionCreatedEvent.ts
@@ -5,7 +5,7 @@ import { TransactionProjection } from "../Projections/TransactionProjection";
 export class TransactionCreatedEvent extends SagaEvent {
   public Amount: number;
   public Transaction: TransactionProjection; // TODO: Convert to ID
-  constructor(sagaName: string, sagaId: any) {
+  constructor(sagaName: string, sagaId: any) { // TODO: Use Saga Class instead.
     super(TransactionCreatedEvent.name, sagaName, sagaId);
   }
   public Publish() {
