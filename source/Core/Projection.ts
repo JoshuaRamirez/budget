71f32064c3e0d729f021ff4a55ec05add9f8d87c
Joshua Ramirez <joshua.ramirez@me.com>
2019-07-31 10:56:56 -0600

    Adding a UserId onto abstract Projection populated by new User stub.

--- source/Core/Projection.ts
+++ source/Core/Projection.ts
@@ -1,9 +1,11 @@
 import { identifier, serializable } from "serializr";
 import { Id } from "./Id";
+import { User } from "./User";
 
 export abstract class Projection {
   @serializable(identifier()) public readonly Id: any;
   @serializable public readonly Name: string;
+  @serializable public readonly UserId: any = User.Id;
   protected constructor(name: string) {
     this.Id = Id.Generate();
     this.Name = name;
