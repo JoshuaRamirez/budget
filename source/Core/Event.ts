77d0262df2dc3191c9712da1026d1b7f6ba90e2c
Joshua Ramirez <joshua.ramirez@me.com>
2019-07-31 10:55:45 -0600

    Adding a UserId onto abstract Event populated by new User stub.

--- source/Core/Event.ts
+++ source/Core/Event.ts
@@ -1,7 +1,9 @@
 import { Id } from "./Id";
+import { User } from "./User";
 
 export abstract class Event {
   public readonly Id: any = Id.Generate();
+  public readonly UserId: any = User.Id;
   public readonly Name: string  = this.constructor.name;
   public abstract Publish(): void;
 }
