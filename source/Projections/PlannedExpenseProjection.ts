7069488339386c43919532781ac7c77ba03b3ad8
Joshua Ramirez <joshua.ramirez@me.com>
2019-08-10 08:41:57 -0600

    Fixing Serialization & implementing new IPlannedTransaction. Added All.

--- source/Projections/PlannedExpenseProjection.ts
+++ source/Projections/PlannedExpenseProjection.ts
@@ -1,17 +1,22 @@
-import { list, primitive, serializable } from "serializr";
+import { date, list, primitive, serializable } from "serializr";
+import { IPlannedTransaction } from "../Core/PlannedTransaction";
 import { Projection } from "../Core/Projection";
 import { ProjectionStore } from "../Core/ProjectionStore";
 
-export class PlannedExpenseProjection extends Projection {
+export class PlannedExpenseProjection extends Projection implements IPlannedTransaction {
   public static Get(id: any): PlannedExpenseProjection {
     return ProjectionStore.Instance.GetProjection(PlannedExpenseProjection, id);
   }
+  public static All(): PlannedExpenseProjection[] {
+    return ProjectionStore.Instance.GetProjections(PlannedExpenseProjection);
+  }
+  @serializable public Amount: number;
   @serializable public Description: string;
   @serializable(list(primitive())) public ExpenseIds: any[];
   @serializable public RepeatPeriod: number;
   @serializable public RepeatMeasurement: string;
   @serializable public RepeatCount: number;
-  @serializable public RepeatStart: Date;
+  @serializable(date()) public RepeatStart: Date;
   constructor() {
     super(PlannedExpenseProjection.name);
     this.ExpenseIds = [];
