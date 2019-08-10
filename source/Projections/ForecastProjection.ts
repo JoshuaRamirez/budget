23bf593d80acfe4e517ea8e591e579eb5bd37926
Joshua Ramirez <joshua.ramirez@me.com>
2019-08-10 08:40:42 -0600

    Adding some access methods and an additional property for links.

--- source/Projections/ForecastProjection.ts
+++ source/Projections/ForecastProjection.ts
@@ -1,18 +1,26 @@
-import { serializable } from "serializr";
+import { date, list, primitive, serializable } from "serializr";
 import { Projection } from "../Core/Projection";
 import { ProjectionStore } from "../Core/ProjectionStore";
 
 export class ForecastProjection extends Projection {
+  public static All(): ForecastProjection[] {
+    return ProjectionStore.Instance.GetProjections(ForecastProjection);
+  }
   public static Get(id: any): ForecastProjection {
     return ProjectionStore.Instance.GetProjection(ForecastProjection, id);
   }
-  @serializable public Amount: number;
-  @serializable public AccountId: any;
-  @serializable public CategoryId: any;
-  @serializable public Date: Date;
-  @serializable public PlannedDepositId: any;
-  @serializable public PlannedForecastId: any;
-  @serializable public Notes: string;
+  public static Last(): ForecastProjection {
+    const result = ProjectionStore.Instance.GetProjections(ForecastProjection);
+    return result[result.length - 1];
+  }
+  @serializable public Amount: number = undefined;
+  @serializable public AccountId: any = undefined;
+  @serializable public CategoryId: any = undefined;
+  @serializable(date()) public Date: Date = undefined;
+  @serializable(list(primitive())) public PlannedDepositIds: any[] = [];
+  @serializable(list(primitive())) public PlannedExpenseIds: any[] = [];
+  @serializable(list(primitive())) public PlannedForecastIds: any[] = [];
+  @serializable public Notes: string = undefined;
   constructor() {
     super(ForecastProjection.name);
   }
