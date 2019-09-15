6e221e1e997189fee4a1d865879b5e4893605d23
Joshua Ramirez <joshua.ramirez@me.com>
2019-09-15 03:11:31 -0700

    Upgraded concept of Id to read only.

--- source/Core/Projection.ts
+++ source/Core/Projection.ts
@@ -1,13 +1,18 @@
 import { identifier, serializable } from "serializr";
 import { Id } from "./Id";
-import { User } from "./User";
 
 export abstract class Projection {
-  @serializable(identifier()) public readonly Id: any;
+  public get Id(): any {
+    return this.id;
+  }
   @serializable public readonly Name: string;
+  @serializable(identifier()) private id: any;
   protected constructor(name: string) {
-    this.Id = Id.Generate();
+    this.id = Id.Generate();
     this.Name = name;
+  }
+  public ReplaceId(id: any) {
+    this.id = id;
   }
   public abstract Project();
   public abstract Update();
