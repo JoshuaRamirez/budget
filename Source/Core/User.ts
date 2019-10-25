f28f7eba88a4c681d4f868285fbabf34444c0dcd
Joshua Ramirez <joshua.ramirez@me.com>
2019-10-25 08:54:40 -0700

    Added a GetDate function that returns today's date at midnight.

--- Source/Core/User.ts
+++ Source/Core/User.ts
@@ -3,4 +3,12 @@ import { Id } from "../Projections/Core/Id";
 export class User {
   // TODO: Consider simulating multi-tenancy
   public static Id: any = Id.Generate();
+  public static GetDate() {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+    return today;
+  }
+  public static GetDateTime() {
+    return new Date();
+  }
 }
