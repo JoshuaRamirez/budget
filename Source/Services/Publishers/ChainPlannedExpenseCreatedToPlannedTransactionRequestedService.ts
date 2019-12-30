2e9d9f19e287c4f266f79e0dc63ff20c61e07c98
Joshua Ramirez <joshua.ramirez@me.com>
2019-12-29 23:53:39 -0800

    Created ChainPlannedExpenseCreatedToPlannedTransactionRequestedService.ts service file.

---
+++ Source/Services/Publishers/ChainPlannedExpenseCreatedToPlannedTransactionRequestedService.ts
@@ -0,0 +1,25 @@
+import { PlannedExpenseCreatedEvent } from "../../Events/PlannedExpenseCreatedEvent";
+import { PlannedTransactionCreationRequestedEvent } from "../../Events/PlannedTransactionCreationRequestedEvent";
+import { PlannedExpenseProjection } from "../../Projections/PlannedExpenseProjection";
+import { EventChain } from "../Core/EventChain";
+import { EventLink } from "../Core/EventLink";
+
+export class ChainPlannedExpenseCreatedToPlannedTransactionRequestedService extends EventChain {
+  public static Instance = new ChainPlannedExpenseCreatedToPlannedTransactionRequestedService();
+  constructor() {
+    super();
+    const eventLink = new EventLink(PlannedExpenseCreatedEvent, (subjectEvent: PlannedExpenseCreatedEvent) => {
+      const subject = PlannedExpenseProjection.Get(subjectEvent.PlannedExpenseId);
+      const targetEvent = new PlannedTransactionCreationRequestedEvent();
+      targetEvent.Amount = subject.Amount;
+      targetEvent.Description = subject.Description;
+      targetEvent.RepeatCount = subject.RepeatCount;
+      targetEvent.RepeatMeasurement = subject.RepeatMeasurement;
+      targetEvent.RepeatPeriod = subject.RepeatPeriod;
+      targetEvent.RepeatStart = subject.RepeatStart;
+      targetEvent.TransactionType = "Expense";
+      return targetEvent;
+    });
+    this.Link(eventLink);
+  }
+}
