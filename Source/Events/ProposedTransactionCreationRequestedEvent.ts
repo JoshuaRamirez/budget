547d5c0e756773866276f73590c71a9d05c57867
Joshua Ramirez <joshua.ramirez@me.com>
2019-12-29 23:33:16 -0800

    Created ProposedTransactionCreationRequestedEvent.ts

---
+++ Source/Events/ProposedTransactionCreationRequestedEvent.ts
@@ -0,0 +1,14 @@
+import { Event } from "./Core/Event";
+import { Publisher } from "./Core/Publisher";
+
+export class ProposedTransactionCreationRequestedEvent extends Event {
+
+  public Amount: number;
+  public Date: Date;
+  public Description: string;
+  public PlannedTransactionId: any;
+  public TransactionType: string;
+  public Publish(): void {
+    Publisher.Instance.Publish(this);
+  }
+}
