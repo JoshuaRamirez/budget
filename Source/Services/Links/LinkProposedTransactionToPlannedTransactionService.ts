e5d6f6a6b41a119918ee9e647967ecb38bf9ba7a
Joshua Ramirez <joshua.ramirez@me.com>
2019-12-29 23:52:56 -0800

    Created LinkProposedTransactionToPlannedTransactionService.ts service file.

---
+++ Source/Services/Links/LinkProposedTransactionToPlannedTransactionService.ts
@@ -0,0 +1,23 @@
+import { ProposedTransactionCreatedEvent } from "../../Events/ProposedTransactionCreatedEvent";
+import { PlannedTransactionProjection } from "../../Projections/PlannedTransactionProjection";
+import { ProposedTransactionProjection } from "../../Projections/ProposedTransactionProjection";
+import { LinkManyToOneDeclaration } from "./Core/LinkManyToOneDeclaration";
+import { LinkService } from "./Core/LinkService";
+
+export class LinkProposedTransactionToPlannedTransactionService extends LinkService<ProposedTransactionCreatedEvent> {
+  public static Instance = new LinkProposedTransactionToPlannedTransactionService();
+  private constructor() {
+    const declaration = new LinkManyToOneDeclaration({
+      EventType: ProposedTransactionCreatedEvent,
+      SubjectType: ProposedTransactionProjection,
+      TargetIdFieldName: "PlannedTransactionId",
+      TargetSubjectIdsFieldName: "ProposedTransactionIds",
+      TargetType: PlannedTransactionProjection,
+      TriggeringSubjectIdFieldName: "ProposedTransactionId",
+    });
+    super(declaration);
+  }
+  public Handle(event: ProposedTransactionCreatedEvent): void {
+    super.Handle(event);
+  }
+}
