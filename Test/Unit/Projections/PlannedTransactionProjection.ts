92004ce517ffb244d1f3c819576bd52a8fb31563
Joshua Ramirez <joshua.ramirez@me.com>
2019-12-29 23:49:24 -0800

    Created PlannedTransactionProjection.ts unit test file.

---
+++ Test/Unit/Projections/PlannedTransactionProjection.ts
@@ -0,0 +1,18 @@
+import { assert } from "chai";
+import "mocha";
+import {PlannedTransactionProjection} from "../../../Source/Projections/PlannedTransactionProjection";
+
+describe("PlannedTransactionProjection", () => {
+
+  it("should import", () => {
+    assert.exists(PlannedTransactionProjection);
+  });
+  it("should instantiate", () => {
+    const plannedTransactionProjection = new PlannedTransactionProjection();
+    assert.exists(plannedTransactionProjection);
+  });
+  it("should get", () => {
+    PlannedTransactionProjection.Get(1);
+  });
+
+});
