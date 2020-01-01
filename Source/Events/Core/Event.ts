a24d53239491eb8bc775d2a0c92b383a763a74f7
Joshua Ramirez <joshua.ramirez@me.com>
2019-12-31 20:31:10 -0800

    Removed TODO

--- Source/Events/Core/Event.ts
+++ Source/Events/Core/Event.ts
@@ -4,7 +4,6 @@ import { Id } from "../../Projections/Core/Id";
 export abstract class Event {
   public readonly Id: any = Id.Generate();
   public readonly PublishingUserId: any = User.Id;
-  // TODO: Rename below to EventName
   public readonly EventName: string  = this.constructor.name;
   public abstract Publish(): void;
 }
