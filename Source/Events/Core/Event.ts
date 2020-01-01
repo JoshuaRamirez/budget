883ae9adc92a7a6cf449320fc303d6a714e712ad
Joshua Ramirez <joshua.ramirez@me.com>
2019-12-31 20:31:29 -0800

    Added TODO

--- Source/Events/Core/Event.ts
+++ Source/Events/Core/Event.ts
@@ -3,6 +3,7 @@ import { Id } from "../../Projections/Core/Id";
 
 export abstract class Event {
   public readonly Id: any = Id.Generate();
+  // TODO: Consider how to use the below field.
   public readonly PublishingUserId: any = User.Id;
   public readonly EventName: string  = this.constructor.name;
   public abstract Publish(): void;
