bc4742a6059f4775cc442e504ccb2f68c97d135d
Joshua Ramirez <joshua.ramirez@me.com>
2020-01-02 22:31:16 -0800

    Added a singleton accessor to the abstract Router class.

--- Source/Services/Core/Router.ts
+++ Source/Services/Core/Router.ts
@@ -3,6 +3,7 @@ import { ISubscriber } from "./ISubscriber";
 import { Route } from "./Route";
 
 export abstract class Router implements ISubscriber {
+  public static Instance: Router = new class extends Router {}();
   private routes: Array<Route<Event, Event>> = [];
   public Link<TSubscriptionEvent extends Event, TPublicationEvent extends Event>(route: Route<TSubscriptionEvent, TPublicationEvent>) {
     this.routes.push(route);
