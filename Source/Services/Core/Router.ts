b46a1111757da10abfc931c8e34cf828050407f4
Joshua Ramirez <joshua.ramirez@me.com>
2020-01-03 23:44:09 -0800

    Removed unnecessary ISubscriber implementation.

--- Source/Services/Core/Router.ts
+++ Source/Services/Core/Router.ts
@@ -1,22 +1,10 @@
 import { Event } from "../../Events/Core/Event";
-import { ISubscriber } from "./ISubscriber";
 import { Route } from "./Route";
 
-export abstract class Router implements ISubscriber {
+export abstract class Router {
   public static Instance: Router = new class extends Router {}();
   private routes: Array<Route<Event, Event>> = [];
   public Link<TSubscriptionEvent extends Event, TPublicationEvent extends Event>(route: Route<TSubscriptionEvent, TPublicationEvent>) {
     this.routes.push(route);
-  }
-  public Subscribe() {
-    this.routes.forEach((route) => {
-      route.Subscribe();
-    });
-  }
-
-  public UnSubscribe() {
-    this.routes.forEach((route) => {
-      route.UnSubscribe();
-    });
   }
 }
