e2204fab912e3973df3e037124deaebae2d6c1b3
Joshua Ramirez <joshua.ramirez@me.com>
2020-01-03 23:46:09 -0800

    Created TransactionProposition.ts

---
+++ Source/Services/Domain/Core/TransactionProposition.ts
@@ -0,0 +1,21 @@
+import { User } from "../../../Core/User";
+import { PlannedTransactionProjection } from "../../../Projections/PlannedTransactionProjection";
+
+export class TransactionProposition {
+  public static GetProposedDate(plannedTransactionProjection: PlannedTransactionProjection): Date {
+    const today = User.GetDate();
+    if (plannedTransactionProjection.RepeatCount >= plannedTransactionProjection.TimesRepeated) { return; }
+    if (plannedTransactionProjection.StartDate > today) { return; }
+    let proposedDate: Date;
+    if (plannedTransactionProjection.ProposedTransactionIds.length === 0) {
+      proposedDate = plannedTransactionProjection.StartDate;
+    }
+    if (plannedTransactionProjection.ProposedTransactionIds.length > 0) {
+      const lastProposedTransaction = PlannedTransactionProjection.Last();
+      const nextDate = lastProposedTransaction.Date;
+      nextDate.setDate(nextDate.getDate() + plannedTransactionProjection.RepeatPeriod);
+      proposedDate = nextDate;
+    }
+    return proposedDate;
+  }
+}
