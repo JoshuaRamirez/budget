1f13663a81a04d6fc04d525837a14bc2bd857be7
Joshua Ramirez <joshua.ramirez@me.com>
2020-01-03 23:43:40 -0800

    Added a new mapper function.

--- Source/Projections/Core/Mappers.ts
+++ Source/Projections/Core/Mappers.ts
@@ -1,4 +1,6 @@
 import { PlannedTransactionRequestedEvent } from "../../Events/Requested/Creation/PlannedTransactionRequestedEvent";
+import { ProposedTransactionRequestedEvent } from "../../Events/Requested/Creation/ProposedTransactionRequestedEvent";
+import { PlannedTransactionProjection } from "../PlannedTransactionProjection";
 import { IPlannedTransaction } from "./IPlannedTransaction";
 
 export const MapPlannedItemToPlannedTransaction = (plannedItem: IPlannedTransaction, transactionType: string) => {
@@ -12,3 +14,11 @@ export const MapPlannedItemToPlannedTransaction = (plannedItem: IPlannedTransact
   plannedTransactionRequestedEvent.TransactionType = transactionType;
   return plannedTransactionRequestedEvent;
 };
+export function MapPlannedTransactionToProposedTransactionCreationRequested(plannedTransactionProjection: PlannedTransactionProjection, proposedDate: Date) {
+  const proposedTransactionCreationRequestedEvent = new ProposedTransactionRequestedEvent();
+  proposedTransactionCreationRequestedEvent.Amount = plannedTransactionProjection.Amount;
+  proposedTransactionCreationRequestedEvent.Description = plannedTransactionProjection.Description;
+  proposedTransactionCreationRequestedEvent.PlannedTransactionId = plannedTransactionProjection.Id;
+  proposedTransactionCreationRequestedEvent.Date = proposedDate;
+  return proposedTransactionCreationRequestedEvent;
+}
