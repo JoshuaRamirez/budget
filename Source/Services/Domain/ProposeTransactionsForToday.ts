062873018c91a9c1c073ad70c9aeecbb6af4ab74
Joshua Ramirez <joshua.ramirez@me.com>
2020-01-03 23:47:33 -0800

    Created ProposeTransactionsForToday.ts

---
+++ Source/Services/Domain/ProposeTransactionsForToday.ts
@@ -0,0 +1,22 @@
+import { DailyTimerIntervalPublished } from "../../Events/System/DailyTimerIntervalPublished";
+import { MapPlannedTransactionToProposedTransactionCreationRequested } from "../../Projections/Core/Mappers";
+import { PlannedTransactionProjection } from "../../Projections/PlannedTransactionProjection";
+import { Receiver } from "../Core/Receiver";
+import { TransactionProposition } from "./Core/TransactionProposition";
+
+export class ProposeTransactionsForToday extends Receiver<DailyTimerIntervalPublished> {
+  public static Instance: ProposeTransactionsForToday = new ProposeTransactionsForToday();
+  constructor() {
+    super(DailyTimerIntervalPublished);
+  }
+  public Receive(event: DailyTimerIntervalPublished): void {
+    const plannedTransactionProjections = PlannedTransactionProjection.All();
+    plannedTransactionProjections.forEach((plannedTransactionProjection) => {
+      const proposedDate = TransactionProposition.GetProposedDate(plannedTransactionProjection);
+      const proposedTransactionCreationRequestedEvent = MapPlannedTransactionToProposedTransactionCreationRequested(plannedTransactionProjection, proposedDate);
+      if (proposedTransactionCreationRequestedEvent) {
+        proposedTransactionCreationRequestedEvent.Publish();
+      }
+    });
+  }
+}
