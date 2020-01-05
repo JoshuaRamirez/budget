2eedce100d9cc1ad273559914460d2a6a4149743
Joshua Ramirez <joshua.ramirez@me.com>
2020-01-05 10:30:53 -0800

    Created IncrementProposedTransactionCounter.ts

---
+++ Source/Services/Domain/IncrementProposedTransactionCounter.ts
@@ -0,0 +1,14 @@
+import { ProposedTransactionCreatedEvent } from "../../Events/Created/ProposedTransactionCreatedEvent";
+import { PlannedTransactionProjection } from "../../Projections/PlannedTransactionProjection";
+import { ProposedTransactionProjection } from "../../Projections/ProposedTransactionProjection";
+import { Receiver } from "../Core/Receiver";
+
+export class IncrementProposedTransactionCounter extends Receiver<ProposedTransactionCreatedEvent> {
+  public static Instance = new IncrementProposedTransactionCounter(ProposedTransactionCreatedEvent);
+  public Receive(event: ProposedTransactionCreatedEvent): void {
+    const proposedTransaction = ProposedTransactionProjection.Get(event.ProposedTransactionId);
+    const plannedTransaction = PlannedTransactionProjection.Get(proposedTransaction.PlannedTransactionId);
+    plannedTransaction.TimesRepeated += 1;
+    plannedTransaction.Update();
+  }
+}
