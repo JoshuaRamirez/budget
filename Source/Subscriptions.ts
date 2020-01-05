09be01117296ecfc5378af04231a9be09abf3779
Joshua Ramirez <joshua.ramirez@me.com>
2020-01-05 10:31:19 -0800

    Added a subscription

--- Source/Subscriptions.ts
+++ Source/Subscriptions.ts
@@ -14,6 +14,7 @@ import { CreateProposedTransactionService } from "./Services/Creates/CreatePropo
 import { CreateTransactionService } from "./Services/Creates/CreateTransactionService";
 import { CreateUserService } from "./Services/Creates/CreateUserService";
 import { ForecastPlannedTransactionsService } from "./Services/Domain/ForecastPlannedTransactionsService";
+import { IncrementProposedTransactionCounter } from "./Services/Domain/IncrementProposedTransactionCounter";
 import { ProposeTransactionsForToday } from "./Services/Domain/ProposeTransactionsForToday";
 import { UpdateLedgerBalanceService } from "./Services/Domain/UpdateLedgerBalanceService";
 import { UpdateLedgerStartingBalanceService } from "./Services/Domain/UpdateLedgerStartingBalanceService";
@@ -65,7 +66,7 @@ export class Subscriptions {
     CreateTransactionService.Instance,
     CreateUserService.Instance
   ];
-  private static updateServices: ISubscriber[] = [UpdateLedgerBalanceService.Instance, UpdateLedgerStartingBalanceService.Instance];
+  private static updateServices: ISubscriber[] = [UpdateLedgerBalanceService.Instance, UpdateLedgerStartingBalanceService.Instance, IncrementProposedTransactionCounter.Instance];
   private static linkServices: ISubscriber[] = [
     LinkAccountToUserService.Instance,
     LinkDepositToPlannedDepositService.Instance,
