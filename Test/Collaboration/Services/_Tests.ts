002eb04b2fae5239ba4ef095a7974451d9115a58
Joshua Ramirez <joshua.ramirez@me.com>
2020-01-06 00:45:54 -0800

    Moved files from Updates to Domain

--- Test/Collaboration/Services/_Tests.ts
+++ Test/Collaboration/Services/_Tests.ts
@@ -2,6 +2,6 @@ describe("Services", () => {
   require("./Creates/_Test");
   require("./Links/_Test");
   require("./Publishers/_Tests");
-  require("./Updates/_Tests");
+  require("./Domain/_Tests");
   require("./ForecastPlannedTransactionsTests");
 });
